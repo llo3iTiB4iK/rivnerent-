5f100478dd91bf1e08cb70e0ec04dd3943231890
llo3iTiB4iK <lyalyaivan2004@gmail.com>
2024-08-03 18:21:48 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/cars/")
+def all_cars():
+    return render_template("cars.html")
+
+
+@app.route("/rules/")
+def rules_n_terms():
+    return render_template("rules.html")
+
+
+@app.route("/about/")
+def about_us():
+    return render_template("about_us.html")
+
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
