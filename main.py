a0c9f80e403668ad3cb5c853aa27b8b350adfe44
llo3iTiB4iK <lyalyaivan2004@gmail.com>
2024-08-06 01:09:00 +0300

    Add Car Addition page, CarForm WTForm, partially implement car addition logic, update requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, URLField, TextAreaField, FloatField, IntegerField
+from wtforms.validators import DataRequired, URL, NumberRange
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -101,7 +101,14 @@ class MyDataRequired(DataRequired):
     def __init__(self, message=None):
         if not message:
             message = 'Це поле обов\'язкове для заповнення'
-        super(MyDataRequired, self).__init__(message=message)
+        super().__init__(message=message)
+
+
+class MyURL(URL):
+    def __init__(self, message=None):
+        if not message:
+            message = 'Некоректне URL-посилання'
+        super().__init__(message=message)
 
 
 class LoginForm(FlaskForm):
@@ -109,6 +116,30 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[MyDataRequired()], render_kw={"placeholder": "Пароль"})
     remember_me = BooleanField('Запам\'ятати мене')
     submit = SubmitField('Увійти')
+
+
+class CarForm(FlaskForm):
+    name = StringField('Марка, модель', validators=[MyDataRequired()], render_kw={"placeholder": "Audi A6 Allroad"})
+    category = SelectField('Категорія', validators=[MyDataRequired()],
+                           choices=[(alias, member.value) for alias, member in CarCategoryEnum.__members__.items()])
+    img_url = URLField('Посилання на зображення', validators=[MyDataRequired(), MyURL()],
+                       render_kw={"placeholder": "https://upload.wikimedia.org/wikipedia/commons/3/3d/Audi_A6_Allroad_Quattro_C8_1X7A0301.jpg"})
+    description = TextAreaField('Опис', validators=[MyDataRequired()], render_kw={"placeholder": "Надійний, елегантний універсал для подорожей з сім'єю."})
+    engine_size = FloatField('Об\'єм двигуна (л)', validators=[MyDataRequired("Об'єм двигуна має бути задано у форматі числа з плаваючою крапкою"),
+                                                               NumberRange(0, message="Літраж двигуна повинен перевищувати 0")], render_kw={"placeholder": "2.0"})
+    fuel_type = SelectField('Тип пального', validators=[MyDataRequired()],
+                           choices=[(alias, member.value) for alias, member in FuelTypeEnum.__members__.items()])
+    transmission = SelectField('Тип КПП', validators=[MyDataRequired()],
+                            choices=[(alias, member.value) for alias, member in TransmissionEnum.__members__.items()])
+    seats = IntegerField('Кількість місць', validators=[MyDataRequired(), NumberRange(1, message="Авто повинне мати хоча б 1 місце")], render_kw={"placeholder": "5"})
+    info_url = URLField('Посилання на детальні характеристики авто', validators=[MyDataRequired(), MyURL()],
+                       render_kw={"placeholder": "https://audi-a6-allroad-quattro.infocar.ua/mod_14642_a6-allroad-quattro_id5893.html"})
+    price_1to3 = StringField('Вартість за добу (термін прокату 1-3 діб)', validators=[MyDataRequired()], render_kw={"placeholder": "999 $"})
+    price_4to9 = StringField('Вартість за добу (термін прокату 4-9 діб)', validators=[MyDataRequired()], render_kw={"placeholder": "999 $"})
+    price_10to25 = StringField('Вартість за добу (термін прокату 10-25 діб)', validators=[MyDataRequired()], render_kw={"placeholder": "999 $"})
+    price_26to89 = StringField('Вартість за добу (термін прокату 26-89 діб)', validators=[MyDataRequired()], render_kw={"placeholder": "999 $"})
+    deposit = StringField('Розмір завдатку', validators=[MyDataRequired()], render_kw={"placeholder": "999 $"})
+    submit = SubmitField('Зберегти')
 
 
 @app.route("/")
@@ -175,10 +206,13 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/add_car/")
+@app.route("/add_car/", methods=["GET", "POST"])
 @login_required
 def add_car():
-    return "add car"
+    form = CarForm()
+    if form.validate_on_submit():
+        pass
+    return render_template("add_car.html", form=form)
 
 
 @app.route("/edit_car/<int:car_id>")
